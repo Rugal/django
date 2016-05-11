ed04fc2922ab414035c77411b29e0e922399be92
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2016-05-10 22:56:53 -0400

    library index

--- library/views.py
+++ library/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from library.models import Book, Dvd, Libuser, Libitem
 
+# Import necessary classes
 # Create your views here.
+def index(request):
+    booklist = Book.objects.all() [:10]
+    response = HttpResponse()
+    heading1 = '<p>' + 'List of books: ' + '</p>'
+    response.write(heading1)
+    for book in booklist:
+        para = '<p>' + str(book) + '</p>'
+        response.write(para)
+    return response
