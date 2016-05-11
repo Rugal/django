4834900efc6d3cbb6e5b4d4c5e2758feab13ed72
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2016-05-11 01:18:18 -0400

    lab5 detail

--- library/urls.py
+++ library/urls.py
@@ -18,4 +18,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^about$', views.about, name='about'),
+    url(r'^(?P<item_id>\d+)$', views.detail, name='detail'),
 ]
--- library/views.py
+++ library/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from library.models import Book, Dvd, Libuser, Libitem
+from django.shortcuts import get_object_or_404
 
 # Import necessary classes
 # Create your views here.
@@ -13,3 +14,26 @@ def index(request):
         para = '<p>' + str(book) + '</p>'
         response.write(para)
     return response
+
+
+def about(request):
+    response = HttpResponse()
+    response.write('This is a Library APP.')
+    return response
+
+def detail(request, item_id):
+    try:
+        item = Libitem.objects.get(id=item_id)
+    except Exception as e:
+        return get_object_or_404(Libitem, id=item_id)
+    response = HttpResponse()
+    response.write('<table>')
+    response.write('<tr><td>title</td><td>'+ item.title +'</td></tr>')
+    response.write('<tr><td>duedate</td><td>'+ str(item.duedate) +'</td></tr>')
+    try:
+        response.write('<tr><td>author</td><td>'+ Book.objects.get(id=item_id).author +'</td></tr>')
+    except Exception as e:
+        response.write('<tr><td>maker</td><td>'+ Dvd.objects.get(id=item_id).maker +'</td></tr>')
+    response.write('</table>')
+    return response
+
