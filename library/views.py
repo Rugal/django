0a47dd53748cc11fd776d16bbcb8724d8982223b
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2016-05-20 23:13:23 -0400

    template

--- library/views.py
+++ library/views.py
@@ -6,34 +6,16 @@ from django.shortcuts import get_object_or_404
 # Import necessary classes
 # Create your views here.
 def index(request):
-    booklist = Book.objects.all() [:10]
-    response = HttpResponse()
-    heading1 = '<p>' + 'List of books: ' + '</p>'
-    response.write(heading1)
-    for book in booklist:
-        para = '<p>' + str(book) + '</p>'
-        response.write(para)
-    return response
-
+    itemlist = Libitem.objects.all().order_by('title')[:10]
+    return render(request, 'library/index.html', {'itemlist': itemlist})
 
 def about(request):
-    response = HttpResponse()
-    response.write('This is a Library APP.')
-    return response
+    return render(request, 'library/about.html')
 
 def detail(request, item_id):
     try:
         item = Libitem.objects.get(id=item_id)
     except Exception as e:
         return get_object_or_404(Libitem, id=item_id)
-    response = HttpResponse()
-    response.write('<table>')
-    response.write('<tr><td>title</td><td>'+ item.title +'</td></tr>')
-    response.write('<tr><td>duedate</td><td>'+ str(item.duedate) +'</td></tr>')
-    try:
-        response.write('<tr><td>author</td><td>'+ Book.objects.get(id=item_id).author +'</td></tr>')
-    except Exception as e:
-        response.write('<tr><td>maker</td><td>'+ Dvd.objects.get(id=item_id).maker +'</td></tr>')
-    response.write('</table>')
-    return response
+    return render(request, 'library/detail.html', {'item':item})
 
